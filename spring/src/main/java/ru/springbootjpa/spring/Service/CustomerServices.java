0616d92babdf92df5233d084edba50bfd2c1c8e1
VitaliyDronenko <buy-matras@yandex.ru>
2023-03-06 13:17:52 +0300

    Добавил в контроллер CustomerController , deleteCustomer

--- spring/src/main/java/ru/springbootjpa/spring/Service/CustomerServices.java
+++ spring/src/main/java/ru/springbootjpa/spring/Service/CustomerServices.java
@@ -43,4 +43,8 @@ public class CustomerServices {
     public Optional<Customer> findByCity(String city) {
         return customerRepository.findCustomerByCity(city);
     }
+
+    public void deleteCustomer(Long id) {
+        customerRepository.deleteById(id);
+    }
 }
